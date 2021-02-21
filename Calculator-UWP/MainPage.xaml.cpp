4765283670cc986d040c00e9157f8eea710288a5
handsomefox <handsomefox.hf@gmail.com>
2021-02-22 01:55:35 +0200

    FIxed some bugs, added a popup message if the input is empty

--- Calculator-UWP/MainPage.xaml.cpp
+++ Calculator-UWP/MainPage.xaml.cpp
@@ -10,13 +10,15 @@ using namespace Calculator_UWP;
 using namespace Platform;
 using namespace Windows::UI::Xaml;
 using namespace Windows::UI::ViewManagement;
+using namespace Windows::UI::Popups;
+
 using std::string;
 using std::to_string;
 using std::wstring;
 
 string input;
 double result = 0.0;
-bool leftBracketCreated = false;
+bool LeftBracketCreated = false;
 Windows::Foundation::Size PrefferedApplicationWindowSize(328, 564);
 
 void MainPage::OnSizeChanged(Object^ sender, SizeChangedEventArgs^ e) {
@@ -24,13 +26,13 @@ void MainPage::OnSizeChanged(Object^ sender, SizeChangedEventArgs^ e) {
     view->TryResizeView(PrefferedApplicationWindowSize);
 }
 
-String^ convertFromString(const string& input) {
+String^ ConvertToString(const string& input) {
     wstring w_str = wstring(input.begin(), input.end());
     const wchar_t* w_chars = w_str.c_str();
     return (ref new String(w_chars, (unsigned int)w_str.length()));
 }
 
-bool CheckForSpecialSymbols(string& input) {
+bool CheckForSpecialSymbols(const string& input) {
     size_t length = input.length() - 1;
     if (input[length] == '*' || input[length] == '/' || input[length] == '+' ||
         input[length] == '-' || input[length] == '%' || input[length] == '.')
@@ -38,7 +40,7 @@ bool CheckForSpecialSymbols(string& input) {
     else
         return false;
 }
-bool CheckForSpecialSymbols(string& input, bool checkForBrackets) {
+bool CheckForSpecialSymbols(const string& input, bool CheckForBrackets) {
     size_t length = input.length() - 1;
     if (input[length] != '*' || input[length] != '/' || input[length] != '+' ||
         input[length] != '-' || input[length] != '(' || input[length] != ')' ||
@@ -47,17 +49,45 @@ bool CheckForSpecialSymbols(string& input, bool checkForBrackets) {
     else
         return false;
 }
+
+void DisplayPopup(const string& popup_msg)
+{
+    Controls::ContentDialog^ popupDialog = ref new Controls::ContentDialog();
+    popupDialog->Title = "Hey there!";
+    popupDialog->Content = ConvertToString(popup_msg);
+    popupDialog->CloseButtonText = "OK";
+    popupDialog->ShowAsync();
+}
+
+bool StrContainsNumber(const string& input) {
+    const string numbers = "1234567890";
+
+    if (input.empty()) {
+        DisplayPopup(string("You need to enter some numbers first!"));
+        return false;
+    }
+    else {
+        for (char i : input)
+            for (char number : numbers)
+                if (i == number)
+                    return true;
+    }
+	DisplayPopup(string("Your input doesn't have any numbers!"));
+    return false;
+}
+
 struct Exponentiation {
 private:
     double m_input = 0.0;
     bool m_calculatingPower = false;
+
+public:
     void Reset() {
         m_input = 0;
         m_calculatingPower = false;
         input.clear();
     }
-
-public:
+	
     void Initialize(double input) {
         m_input = input;
         m_calculatingPower = true;
@@ -69,17 +99,16 @@ public:
     };
 
     bool GetPowerState() { return m_calculatingPower; };
-} s_powerOf;
+} s_Exponentiation;
 
 MainPage::MainPage() {
     InitializeComponent();
-    auto view = ApplicationView::GetForCurrentView();
-    view->TryResizeView(PrefferedApplicationWindowSize);
-    view->SetPreferredMinSize(PrefferedApplicationWindowSize);
+    ApplicationView::GetForCurrentView()->SetPreferredMinSize(
+        PrefferedApplicationWindowSize);
 }
 
 void MainPage::UpdateCalculatorOutput() {
-    this->CalculatorOutputTextBox->Text = convertFromString(input);
+    this->CalculatorOutputTextBox->Text = ConvertToString(input);
 }
 
 double CalculateExpression() { return te_interp(input.c_str(), 0); }
@@ -98,7 +127,8 @@ void MainPage::RemoveTrailingZeroesAndUpdate(double result) {
 void MainPage::ButtonClear_Click(Object^ sender, RoutedEventArgs^ e) {
     this->CalculatorOutputTextBox->Text = "";
     this->CalculatorOutputTextBox->PlaceholderText = "0";
-    leftBracketCreated = false;
+    LeftBracketCreated = false;
+    s_Exponentiation.Reset();
     input.clear();
 }
 void MainPage::Button1_Click(Object^ sender, RoutedEventArgs^ e) {
@@ -143,8 +173,11 @@ void MainPage::Button0_Click(Object^ sender, RoutedEventArgs^ e) {
 }
 void MainPage::Button_Addition_Click(Object^ sender, RoutedEventArgs^ e) {
     if (input.length() > 0) {
-        if (CheckForSpecialSymbols(input))
+        if (CheckForSpecialSymbols(input)) {
+            if (input.length() == 1)
+                return;
             input[input.length() - 1] = '+';
+        }
         else
             input += '+';
     }
@@ -163,18 +196,23 @@ void MainPage::Button_Subtraction_Click(Object^ sender, RoutedEventArgs^ e) {
 }
 void MainPage::Button_Division_Click(Object^ sender, RoutedEventArgs^ e) {
     if (input.length() > 0) {
-        if (CheckForSpecialSymbols(input))
+        if (CheckForSpecialSymbols(input)) {
+            if (input.length() == 1)
+                return;
             input[input.length() - 1] = '/';
+        }
         else
             input += '/';
     }
     UpdateCalculatorOutput();
 }
-void MainPage::Button_Multiplication_Click(Object^ sender,
-    RoutedEventArgs^ e) {
+void MainPage::Button_Multiplication_Click(Object^ sender, RoutedEventArgs^ e) {
     if (input.length() > 0) {
-        if (CheckForSpecialSymbols(input))
+        if (CheckForSpecialSymbols(input)) {
+            if (input.length() == 1)
+                return;
             input[input.length() - 1] = '*';
+        }
         else
             input += '*';
     }
@@ -182,8 +220,8 @@ void MainPage::Button_Multiplication_Click(Object^ sender,
 }
 void MainPage::Button_Equals_Click(Object^ sender, RoutedEventArgs^ e) {
     double result = 0.0;
-    if (s_powerOf.GetPowerState() == true) {
-        result = s_powerOf.Calculate(CalculateExpression());
+    if (s_Exponentiation.GetPowerState() == true) {
+        result = s_Exponentiation.Calculate(CalculateExpression());
         this->CalculatorOutputTextBox->PlaceholderText = "0";
     }
     else
@@ -191,15 +229,15 @@ void MainPage::Button_Equals_Click(Object^ sender, RoutedEventArgs^ e) {
     RemoveTrailingZeroesAndUpdate(result);
 }
 void MainPage::Button_Bracket_Click(Object^ sender, RoutedEventArgs^ e) {
-    if (leftBracketCreated == false)
+    if (LeftBracketCreated == false)
         input += "(";
     else
         input += ")";
-    leftBracketCreated = !leftBracketCreated;
+    LeftBracketCreated = !LeftBracketCreated;
     UpdateCalculatorOutput();
 }
 void MainPage::Button_Sq_Root_Click(Object^ sender, RoutedEventArgs^ e) {
-    if (input.length() > 0) {
+    if (StrContainsNumber(input)) {
         double result = sqrt(CalculateExpression());
         RemoveTrailingZeroesAndUpdate(result);
     }
@@ -210,15 +248,18 @@ void MainPage::Button_Back_Click(Object^ sender, RoutedEventArgs^ e) {
     UpdateCalculatorOutput();
 }
 void MainPage::Button_Sqr_Click(Object^ sender, RoutedEventArgs^ e) {
-    if (input.length() > 0) {
+    if (StrContainsNumber(input)) {
         double result = (CalculateExpression() * CalculateExpression());
         RemoveTrailingZeroesAndUpdate(result);
     }
 }
 void MainPage::Button_Dot_Click(Object^ sender, RoutedEventArgs^ e) {
     if (input.length() > 0) {
-        if (CheckForSpecialSymbols(input))
+        if (CheckForSpecialSymbols(input)) {
+            if (input.length() == 1)
+                return;
             input[input.length() - 1] = '.';
+        }
         else
             input += '.';
     }
@@ -229,24 +270,27 @@ void MainPage::Button_Dot_Click(Object^ sender, RoutedEventArgs^ e) {
 
 void MainPage::Button_Mod_Click(Object^ sender, RoutedEventArgs^ e) {
     if (input.length() > 0) {
-        if (CheckForSpecialSymbols(input))
+        if (CheckForSpecialSymbols(input)) {
+            if (input.length() == 1)
+                return;
             input[input.length() - 1] = '%';
+        }
         else
             input += '%';
     }
     UpdateCalculatorOutput();
 }
 void MainPage::Button_ln_Click(Object^ sender, RoutedEventArgs^ e) {
-    if (input.length() > 0) {
+    if (StrContainsNumber(input)) {
         double result = (log(CalculateExpression()));
         RemoveTrailingZeroesAndUpdate(result);
     }
 }
 void MainPage::Button_powerOf_Click(Object^ sender, RoutedEventArgs^ e) {
-    if (input.length() > 0) {
+    if (StrContainsNumber(input)) {
         if (CheckForSpecialSymbols(input, 1)) {
             double value_to_calculate = (CalculateExpression());
-            s_powerOf.Initialize(value_to_calculate);
+            s_Exponentiation.Initialize(value_to_calculate);
             input.clear();
             this->CalculatorOutputTextBox->PlaceholderText = "Enter needed power: ";
             UpdateCalculatorOutput();
@@ -254,7 +298,7 @@ void MainPage::Button_powerOf_Click(Object^ sender, RoutedEventArgs^ e) {
     }
 }
 void MainPage::Button_Inverse_Click(Object^ sender, RoutedEventArgs^ e) {
-    if (input.length() > 0) {
+    if (StrContainsNumber(input)) {
         double result = 1 / (CalculateExpression());
         RemoveTrailingZeroesAndUpdate(result);
     }
